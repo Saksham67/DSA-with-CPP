992cd1c46110cc37c0a3aac4506c5f9f1c60f4d9
Saksham67 <sakshamkumar652547@gmail.com>
2024-12-14 22:57:00 +0530

    second commit

---
+++ Arrays/second_largest.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>     //second largest element in any array
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the no. of elements: "<<endl;
+    cin>>n;
+    cout<<"Enter the elements value: "<<endl;
+    int arr[n];
+    for(int i= 0; i<n; i++){
+        cin>>arr[i];
+    }
+    int largest = arr[0];
+    for(int j=1 ; j<=n; j++){
+        if(largest< arr[j]) largest = arr[j];
+    }
+    cout<<"first largest element is: "<<largest<<endl;
+    return 0;
+
+
+}
