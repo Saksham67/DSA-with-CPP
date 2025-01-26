8dac3e618c81786660f1aae78cc2620795a1cd9b
Saksham67 <sakshamkumar652547@gmail.com>
2025-01-26 22:54:09 +0530

    updated

---
+++ Basics/Square.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int FindSquareroot(int x)
+{
+    int root = 0;
+    for(int i = 1; i * i <= x; i++)
+    {
+        root = i;
+
+    }
+    return root;
+}
+int main()
+{
+    int num;
+    cout<<"Enter the number you want to SquareRoot: "<<endl;
+    cin>>num;
+    cout<<"The SquareRoot of "<<num<<" is: "<<FindSquareroot(num);
+    return 0;
+
+}
