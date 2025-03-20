37ec84050c06d5cdcca4511d6c9456aa5cb09d5d
Saksham67 <sakshamkumar652547@gmail.com>
2025-03-20 12:23:18 +0530

    updated

--- Sorting&Searching/problem2.cpp
+++ Sorting&Searching/problem2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
       int n, mid, L, H;
-      cou<<"Enter size of array: "<<endl;
+      cout<<"Enter size of array: "<<endl;
       cin>>n;
       int arr[n];
       cout<<"enter elements: "<<endl;
