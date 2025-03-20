9e2e397cce88acccadfbf1c897ad3ae45f4b7e26
Saksham67 <sakshamkumar652547@gmail.com>
2025-03-20 12:09:03 +0530

    updated

--- Sorting&Searching/problem1.cpp
+++ Sorting&Searching/problem1.cpp
@@ -10,7 +10,7 @@ int FindElement(int num)
     for(int i=0; i<size; i++){
         cin>>arr[i];
     }
-    for(int i = 0; i<=size; i++)
+    for(int i = 0; i<size; i++)
     {
         if(num == arr[i])
         {
