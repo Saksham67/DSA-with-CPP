bb9e0ca649f5813c26e477ca6b81c503327c81f7
Saksham67 <sakshamkumar652547@gmail.com>
2025-03-20 11:17:12 +0530

    updated

--- Sorting&Searching/problem1.cpp
+++ Sorting&Searching/problem1.cpp
@@ -1,17 +1,32 @@
 #include<bits/stdc++.h>     //Linear Search
 using namespace std;
-int main()
+int FindElement(int num)
 {
-    int n;
+    int size;
     cout<<"Enter Length of array: "<<endl;
-    cin>>n;
-    int arr[n];
-    for(int i=0; i<n; i++){
+    cin>>size;
+    int arr[size];
+    cout<<"Enter Elements: "<<endl;
+    for(int i=0; i<size; i++){
         cin>>arr[i];
     }
-    
-    
+    for(int i = 0; i<=size; i++)
+    {
+        if(num == arr[i])
+        {
+            cout<<"Element Found at index: "<<i;
+        }
+    }
+
+}
+int main()
+{   
+
+    int num;
+    cout<<"Enter a Number, you want to find: "<<endl;
+    cin>>num;
+    FindElement(num);
 
 
-
+ 
 }
