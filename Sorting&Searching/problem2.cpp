ff882a36a3d535ec8b84c83e62bbcc96f9f53173
Saksham67 <sakshamkumar652547@gmail.com>
2025-03-21 18:25:06 +0530

    updated

--- Sorting&Searching/problem2.cpp
+++ Sorting&Searching/problem2.cpp
@@ -1,27 +1,25 @@
-#include<bits/stdc++.h>         //Binary Search
+#include <bits/stdc++.h> //Binary Search
 using namespace std;
 int main()
 {
-      int n, mid, L, H;
-      cout<<"Enter size of array: "<<endl;
-      cin>>n;
-      int arr[n];
-      cout<<"enter elements: "<<endl;
-      for(int i =0 ; i<n; i++)
-      {
-        cin>>arr[i];
-      }
+    int num, mid, L, H;
+    cout << "Enter size of array: " << endl;
+    cin >> num;
+    int arr[num];
+    cout << "enter elements: " << endl;
+    for (int i = 0; i < num; i++)
+    {
+        cin >> arr[i];
+    }
 
-      L = arr[0];
-      H = arr[n-1];
-      mid = (L+H)/2;
+    L = 0;
+    H = num - 1;
+    mid = (L + H) / 2;
 
-      for(int i = 0; i<n; i++)
-      {
-        if(arr[mid]== n) cout<<"Found At: "<<arr[i];
-      }
-      
-
-      
-
+    while (L <= H)
+    {
+        if (arr[mid] == num)
+            cout << "found out at: " << mid;
+        
+    }
 }
