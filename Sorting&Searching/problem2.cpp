c0fe234f3b75278de60cb5ebafa76e8f83f34870
Saksham67 <sakshamkumar652547@gmail.com>
2025-03-21 18:42:23 +0530

    updated

--- Sorting&Searching/problem2.cpp
+++ Sorting&Searching/problem2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    int num, mid, L, H;
+    int num, mid, L, H, target ;
     cout << "Enter size of array: " << endl;
     cin >> num;
     int arr[num];
@@ -11,6 +11,9 @@ int main()
     {
         cin >> arr[i];
     }
+    cout<<"Enter target value to find: "<<endl;
+    cin>>target;
+
 
     L = 0;
     H = num - 1;
@@ -18,17 +21,18 @@ int main()
 
     while (L <= H)
     {
-        if (arr[mid] == num)
+        if (arr[mid] == target)
         {
             cout << "element found out at Index : " << mid;
             break;
         }
-        if(arr[mid] < num)
+        if (arr[mid] < target)
         {
             L = mid + 1;
-        } 
-        else H = mid - 1;
-        
+        }
+        else
+            H = mid - 1;
+        mid = (L+H)/2;
     }
     return -1;
 }
