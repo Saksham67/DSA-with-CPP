5c3bf89bc2528e512ea5d6e3566276c63c304684
Saksham67 <sakshamkumar652547@gmail.com>
2025-04-29 12:15:08 +0530

    updated

--- Patterns/b.cpp
+++ Patterns/b.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>         //i think it's knows as half pyramid pattern
+#include<bits/stdc++.h>         //i think it's known as half pyramid pattern
 using namespace std;
 int main()
 {
