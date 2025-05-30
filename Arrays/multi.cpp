5f58fc14a968a8acde3d1ccad7a91a7521fb05bc
Saksham67 <sakshamkumar652547@gmail.com>
2025-05-30 22:46:45 +0530

    some changes

--- Arrays/multi.cpp
+++ Arrays/multi.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    int arr1[3][2], arr2[2][3];
+    int arr1[3][2], arr2[2][3], multi[3][3];
     int i, j;
     cout << "Taking input for arr1:" << endl;
     for (i = 0; i < 3; i++)
@@ -26,7 +26,7 @@ int main()
     {
         for (j = 0; j < 3; j++)
         {
-            int multi[i][j] = arr1[i][j] * arr2[i][j];
+            multi[i][j] = arr1[i][j] * arr2[i][j];
         }
     }
 }
