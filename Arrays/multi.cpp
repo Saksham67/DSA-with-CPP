53d2231b6cbc378bc9723261c5659581826759ef
Saksham67 <sakshamkumar652547@gmail.com>
2025-05-30 22:39:41 +0530

    some changes

--- Arrays/multi.cpp
+++ Arrays/multi.cpp
@@ -1,34 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     int arr1[3][2], arr2[2][3];
-    int i,j;
-    cout<<"Taking input for arr1:"<<endl;
-    for(i = 0 ; i < 3; i++ )
+    int i, j;
+    cout << "Taking input for arr1:" << endl;
+    for (i = 0; i < 3; i++)
     {
-        for(j = 0; j<2; j++)
+        for (j = 0; j < 2; j++)
         {
-            cin>>arr1[i][j];
+            cin >> arr1[i][j];
         }
     }
 
-    cout<<"Taking input for arr2:"<<endl;
+    cout << "Taking input for arr2:" << endl;
 
-    for(i = 0 ; i < 3; i++ )
+    for (i = 0; i < 3; i++)
     {
-        for(j = 0; j<2; j++)
+        for (j = 0; j < 2; j++)
         {
-            cin>>arr2[i][j];
+            cin >> arr2[i][j];
         }
     }
-    for(i = 0 ; i < 3; i++ )
+    for (i = 0; i < 3; i++)
     {
-        for(j = 0; j<3; j++)
+        for (j = 0; j < 3; j++)
         {
             int multi[i][j] = arr1[i][j] * arr2[i][j];
         }
     }
-    
-
 }
