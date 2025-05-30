5336c6fddd82c8c05ce037139a1085ea71d3a702
Saksham67 <sakshamkumar652547@gmail.com>
2025-05-30 22:37:58 +0530

    modified files

--- Arrays/largest_element.cpp
+++ Arrays/largest_element.cpp
@@ -1,24 +1,24 @@
-#include<bits/stdc++.h>     //largest element in an array.
+#include <bits/stdc++.h> //largest element in an array.
 using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter the no. of elements: "<<endl;
-    cin>>n;
+    cout << "Enter the no. of elements: " << endl;
+    cin >> n;
     int arr[n];
-    cout<<"Enter Elements: "<<endl;
-    for(int i = 0; i<n; i++)
+    cout << "Enter Elements: " << endl;
+    for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
     int largest = arr[0];
-    for(int j = 1; j<n; j++)
+    for (int j = 1; j < n; j++)
     {
-        if(largest < arr[j]){
+        if (largest < arr[j])
+        {
             largest = arr[j];
         }
     }
-    cout<<"The largest element is: "<<largest;
+    cout << "The largest element is: " << largest;
     return 0;
-
 }
