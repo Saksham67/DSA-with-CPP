87a549d114d54039892e2bb1d237686a4dbb1fe5
Saksham67 <sakshamkumar652547@gmail.com>
2025-05-31 15:33:54 +0530

    some changes

--- Arrays/multi.cpp
+++ Arrays/multi.cpp
@@ -4,7 +4,7 @@ int main()
 {
     int arr1[3][2], arr2[2][3], multi[3][3];
     int i, j;
-    cout << "Taking input for arr1:" << endl;
+    cout << "input for arr1:" << endl;
     for (i = 0; i < 3; i++)
     {
         for (j = 0; j < 2; j++)
