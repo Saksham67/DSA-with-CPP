76ccd45a888fa41fdda345540e72f335b6944606
Saksham67 <sakshamkumar652547@gmail.com>
2025-06-02 19:16:38 +0530

    updated file

--- Arrays/smallest_element.cpp
+++ Arrays/smallest_element.cpp
@@ -1,23 +1,24 @@
-#include<bits/stdc++.h>     //smallest element in an array.
+#include <bits/stdc++.h> //smallest element in an array.
 using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter the no. of elements: "<<endl;
-    cin>>n;
+    cout << "Enter the no. of elements: " << endl;
+    cin >> n;
     int arr[n];
-    cout<<"Enter Elements: "<<endl;
-    for(int i = 0; i<n; i++)
+    cout << "Enter Elements: " << endl;
+    for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
     int smallest = arr[0];
-    for(int j = 1; j<n; j++)
+    for (int j = 1; j < n; j++)
     {
-        if(smallest > arr[j]){
+        if (smallest > arr[j])
+        {
             smallest = arr[j];
         }
     }
-    cout<<"The smallest element is: "<<smallest;
+    cout << "The smallest element is: " << smallest;
     return 0;
 }
