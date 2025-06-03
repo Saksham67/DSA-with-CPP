c1081a0e9befbc01945a3dafd88ff7e2004c1f46
Saksham67 <sakshamkumar652547@gmail.com>
2025-06-04 01:51:54 +0530

    added new file

---
+++ Arrays/MaxNum.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>     //finding maximum number in an array
+using namespace std;
+int main()
+{
+    int size;
+    int nums[size];
+    cout<<"Enter size of the array: ";
+    cin>>size;
+    
+
+}
