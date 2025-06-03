fabd621ed2522a8e2a44cc5c6d7b66080906a9ce
Saksham67 <sakshamkumar652547@gmail.com>
2025-06-03 11:52:49 +0530

    modified file

--- Arrays/TwoSum.cpp
+++ Arrays/TwoSum.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 int main(){
     vector<int> twoSum(vector<int>& nums, int target){
-        for(int i = 0; i< nums.size() - 1; i++ 
-    {
-        
-    })
+        for(int i = 0; i< nums.size() - 1; i++ ){
+            for (int j = i+1; j< nums.size(); j++){
+                if(nums[i] + nums[j] == target)
+            }
+        }
 
     }
 }
