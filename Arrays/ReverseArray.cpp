d57b88b213402da0bf4abb52d93dff5c1f7628a7
Saksham67 <sakshamkumar652547@gmail.com>
2025-06-04 02:00:26 +0530

    added new file'

---
+++ Arrays/ReverseArray.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>     //reversing array's element
+using namespace std;
+int main()
+{
+    int size;
+    int nums[size];
+    cout<<"Enter size of the array: ";
+    cin>>size;
+    cout<<"taking input elements: ";
+    for(int i = 0; i< size; i++){
+        cin>>nums[i];
+    }
+
+    
+}
