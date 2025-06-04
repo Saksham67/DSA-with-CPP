0387c9169c5ddd83c4d88ffe578cb68b59405972
Saksham67 <sakshamkumar652547@gmail.com>
2025-06-05 03:03:26 +0530

    updated file

--- Arrays/TwoSum.cpp
+++ Arrays/TwoSum.cpp
@@ -1,23 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h> //TwoSum problem from leetcode.
 using namespace std;
-vector<int> twoSum(vector<int>& nums, int target){
-    for(int i = 0; i< nums.size() - 1; i++ ){
-        for (int j = i+1; j< nums.size(); j++){
-            if(nums[i] + nums[j] == target) return {i, j};
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    for (int i = 0; i < nums.size() - 1; i++)
+    {
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            if (nums[i] + nums[j] == target)
+                return {i, j};
         }
     }
     return {};
-
 }
-int main(){
+int main()
+{
     int size, target;
-    vector<int> arr[size];
-    cout<<"Enter size: "<<endl;
-    cin>>size;
-    cout<<"enter elements: "<<endl;
-    for(int i = 0; i < size; i++){
-        cin>>arr[i];
+    cout << "Enter size: " << endl;
+    cin >> size;
+    vector<int> nums(size);
+    cout << "enter elements: " << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> nums[i];
     }
-    twoSum(arr[size], target);
+    cout<<"Enter target: ";
+    cin>>target;
+    vector<int> result = twoSum(nums, target);
+    if(!result.empty() == target){
 
+        cout << result[0]<<" "<< result[1];
+    }
+    return 0;
 }
