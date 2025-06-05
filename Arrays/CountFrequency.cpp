742a5a76ebee2ff4be35a11deef940ec4723d66b
Saksham67 <sakshamkumar652547@gmail.com>
2025-06-05 19:47:57 +0530

    added new file

---
+++ Arrays/CountFrequency.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> //Given an array and a specific number, count how many times that number appears in the array.
+using namespace std;
+int CountFrequency(int nums[], int size, int target)
+{
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (target == nums[i])
+            count++;
+    }
+    return count;
+}
+int main()
+{
+    int size, target;
+    cout << "Enter size of Array: ";
+    cin >> size;
+    int nums[size];
+    cout << "Enter Array Elements: ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> nums[i];
+    }
+    cout << "Enter a number to count: ";
+    cin >> target;
+    cout << target << "appears " << CountFrequency << "times in the array";
+}
