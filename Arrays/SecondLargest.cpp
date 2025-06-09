4818c7dd36058c468492c3538cb9e3b488c14506
Saksham67 <sakshamkumar652547@gmail.com>
2025-06-09 22:54:41 +0530

    optimized version

--- Arrays/SecondLargest.cpp
+++ Arrays/SecondLargest.cpp
@@ -1,13 +1,6 @@
 #include <bits/stdc++.h>        //second largest element in any array
 #include<algorithm> 
 using namespace std;
-int SecondLargest(int nums[], int size)
-{   
-    sort(nums.begin(), nums.end());
-    return nums[size - 1];
-    
-
-}
 int main()
 {
     int n;
@@ -15,10 +8,12 @@ int main()
     cin >> n;
     cout << "Enter elements : " << endl;
     int arr[n];
+    
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
+    int max1 = arr[0], max2= arr[1];
     int max = arr[0];
     for (int j = 1; j <= n; j++)
     {
@@ -28,6 +23,6 @@ int main()
         }
     }
     cout <<"first largest element is: " << max << endl;
-    cout<<"Second Largest Elements is: "<<SecondLargest(arr, n);
+    
     return 0;
 }
