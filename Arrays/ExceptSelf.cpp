ce4c696cf4319ebfd21b79ba66660891ab3a6aed
Saksham67 <sakshamkumar652547@gmail.com>
2025-06-11 01:42:37 +0530

    final code

--- Arrays/ExceptSelf.cpp
+++ Arrays/ExceptSelf.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 vector<int> productExceptSelf(vector<int> & nums){
     int n = nums.size();
-    vector<int> result();
+    vector<int> result(n, 1);
     for(int i = 0; i < n; i++){
         for( int j = 0; j< n; j++){
             if(i != j){
-                result[] *= nums[i];
+                result[i] *= nums[i];
             }
         }
     }
