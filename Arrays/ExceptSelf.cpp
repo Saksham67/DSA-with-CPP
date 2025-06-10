6233c76cbde7ac6bcf7475f663fc2e8c5776e9d1
Saksham67 <sakshamkumar652547@gmail.com>
2025-06-10 23:56:57 +0530

    added new file

--- Arrays/ExceptSelf.cpp
+++ Arrays/ExceptSelf.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 vector<int> productExceptSelf(vector<int> & nums){
     int n = nums.size();
-    int result[];
+    vector<int> result();
+    for(int i = 0; i < n; i++){
+        for( int j = 0; j< n; j++){
+            if(i != j){
+                result[] *= nums[i];
+            }
+        }
+    }
 }
 
 int main()
