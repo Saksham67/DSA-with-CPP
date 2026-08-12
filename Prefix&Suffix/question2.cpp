ee3b35efb68e6bdce65258f3e36580c64464e7c2
Saksham67 <sakshamkumar652547@gmail.com>
2026-08-13 02:18:48 +0530

    kadane's algo

--- Prefix&Suffix/question2.cpp
+++ Prefix&Suffix/question2.cpp
@@ -1,17 +1,30 @@
-//Largest sum of contiguous subarray
-#include<bits/stdc++.h>
+// Largest sum of contiguous subarray
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    vector<int> v = {5,-8,4,-2,10,-1};
+int main()
+{
+    vector<int> v = {5, -8, 4, -2, 10, -1};
     int n = v.size();
     int maxi = INT_MIN;
-    for(int i = 0; i< n; i++){
-        int sum = 0; 
-        for(int j = i; j< n; j++){
-            sum += v[j];
-            maxi = max(maxi, sum);
-        }
+
+    // Brute force;  Time Complexity O(N^2)
+    //  for(int i = 0; i< n; i++){
+    //      int sum = 0;
+    //      for(int j = i; j< n; j++){
+    //          sum += v[j];
+    //          maxi = max(maxi, sum);
+    //      }
+    //  }
+
+    // Optimal(Kadane's Algorithm)  Time Complexity: O(N)
+    int prefix = 0;
+    for (int x : v)
+    {
+        prefix += x;
+        maxi = max(maxi, prefix);
+        if (prefix < 0)
+            prefix = 0;
     }
-    cout<<maxi;
+    cout << maxi;
     return 0;
 }
