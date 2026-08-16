5cd888fe0d30695294c0eccacc00a8d00225ed00
Saksham67 <sakshamkumar652547@gmail.com>
2026-08-17 03:23:34 +0530

    added new file

---
+++ Prefix&Suffix/question3.cpp
@@ -0,0 +1,14 @@
+//max difference between two elements(with suffix max)
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> arr = {9,5,8,12,2,3,7,4};
+    int n = arr.size();
+    vector<int> suffixMax(n);
+    int maxi = INT_MIN;
+    for(int i = n-1; i>= 0; i--){
+        maxi = max(maxi,arr[i]);
+        suffixMax[i] = maxi;
+    }
+    
+}
